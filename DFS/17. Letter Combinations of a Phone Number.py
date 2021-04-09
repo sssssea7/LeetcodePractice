26b27da73527f212f10a2f74fb26afef03eaff4e
sssssea7 <zhqx9019@gmail.com>
2021-04-08 22:23:17 -0500

    update

---
+++ DFS/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        table = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        self.ans = []
+        def dfs(curr_s, d):
+            if not d:
+                self.ans.append(curr_s)
+                return
+            for c in table[d[0]]:
+                dfs(curr_s+c, d[1:])
+        dfs("", digits)
+        return self.ans
