bdbe095c4d7b5dd3cf4101e67bba65d532997067
sssssea7 <zhqx9019@gmail.com>
2021-05-20 22:23:56 -0500

    update

---
+++ LinkedList/114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.prev = None
+        def dfs(node: TreeNode) -> None:
+            if not node: return
+            dfs(node.right)
+            dfs(node.left)
+            node.right = self.prev
+            node.left = None
+            self.prev = node
+        dfs(root)
