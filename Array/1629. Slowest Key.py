1bae98704039d8e83a66830a2d923a02bafc7abd
sssssea7 <zhqx9019@gmail.com>
2021-09-06 19:59:27 -0500

    update

--- Array/1629. Slowest Key.py
+++ Array/1629. Slowest Key.py
@@ -2,7 +2,6 @@ class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
         ans = keysPressed[0]
         maxTime = releaseTimes[0]
-        T = [releaseTimes[0]] + [0]*(len(keysPressed)-1)
         for i in range(1, len(releaseTimes)):
             curTime = releaseTimes[i]-releaseTimes[i-1]
             if curTime == maxTime:
