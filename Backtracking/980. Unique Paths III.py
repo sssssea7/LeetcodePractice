91c57f360acb63371fc0ddba1512eff4e51263a4
sssssea7 <zhqx9019@gmail.com>
2021-11-02 17:28:56 -0500

    update

---
+++ Backtracking/980. Unique Paths III.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePathsIII(self, A: List[List[int]]) -> int:
+        M, N = len(A), len(A[0])
+        path = set()
+        for i in range(M):
+            for j in range(N):
+                if A[i][j]==0: path.add((i, j))
+                if A[i][j]==1: s = [i, j]
+                if A[i][j]==2: 
+                    e = [i, j]
+                    path.add((i, j))
+        D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def dfs(x, y, path):
+            if [x, y] == e and len(path)==0:
+                return 1
+            ans = 0
+            for dx, dy in D:
+                if (x+dx, y+dy) in path:
+                    path.remove((x+dx, y+dy))
+                    ans += dfs(x+dx, y+dy, path)
+                    path.add((x+dx, y+dy))
+            return ans
+        return dfs(s[0], s[1], path)
