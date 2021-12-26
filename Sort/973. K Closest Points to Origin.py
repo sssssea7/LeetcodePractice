56071571618779b9d2991ca69d556932c73fad19
sssssea7 <zhqx9019@gmail.com>
2021-12-26 02:39:15 -0600

    update

--- Sort/973. K Closest Points to Origin.py
+++ Sort/973. K Closest Points to Origin.py
@@ -4,3 +4,8 @@ class Solution:
             return x[0]**2+x[1]**2
         P.sort(key=square)
         return P[:k]
+
+class Solution:
+    def kClosest(self, P: List[List[int]], k: int) -> List[List[int]]:
+        P.sort(key=lambda x: x[0]**2+x[1]**2)
+        return P[:k]
