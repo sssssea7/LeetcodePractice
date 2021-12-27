228be8e039d8af6e811d651d90d074dc056a2ca4
sssssea7 <zhqx9019@gmail.com>
2021-12-27 01:10:47 -0600

    update

--- BitManipulation/476. Number Complement.py
+++ BitManipulation/476. Number Complement.py
@@ -6,3 +6,17 @@ class Solution:
             ans += "1" if b[i]=="0" else "0"
         return int(ans, 2)
         
+
+class Solution:
+    def findComplement(self, n: int) -> int:
+        k = 0
+        while 2**k<=n: k += 1
+        mask = 2**k-1
+        return n^mask
+
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask = 0
+        while mask < num: mask = (mask << 1) + 1
+        return num ^ mask
