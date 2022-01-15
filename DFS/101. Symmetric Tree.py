5adf7ca0dae7f379c69dc683914f5c13dc0f1e4f
sssssea7 <zhqx9019@gmail.com>
2022-01-15 03:03:16 -0600

    update

---
+++ DFS/101. Symmetric Tree.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(l, r):
+            if not l or not r: return l == r
+            return l.val == r.val and dfs(l.left, r.right) and dfs(l.right, r.left)
+        return dfs(root.left, root.right)
