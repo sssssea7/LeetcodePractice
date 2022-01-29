a10d9a9dbff9d197237e32cf8adf2ac8c918f1f0
sssssea7 <zhqx9019@gmail.com>
2022-01-29 16:51:04 -0600

    update

--- DynamicPrograming/300. Longest Increasing Subsequence.py
+++ DynamicPrograming/300. Longest Increasing Subsequence.py
@@ -5,3 +5,16 @@ class Solution:
             for j in range(i):
                 if nums[i] > nums[j]: dp[i] = max(dp[i], dp[j]+1)
         return max(dp)
+
+
+class Solution:
+    def lengthOfLIS(self, A: List[int]) -> int:
+        @cache
+        def fn(i):
+            if i>=len(A): return 0
+            ans = 0
+            for j in range(i):
+                if A[j]<A[i]:
+                    ans = max(ans, fn(j))
+            return ans+1
+        return max(fn(i) for i in range(len(A)))
