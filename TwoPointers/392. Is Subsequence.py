c8fd002a38056ea7ec149e7699157ea60c416247
sssssea7 <zhqx9019@gmail.com>
2022-03-01 19:18:31 -0600

    update

---
+++ TwoPointers/392. Is Subsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s: return True
+        i = 0
+        for j in range(len(t)):
+            if s[i]==t[j]:
+                i += 1
+            if i==len(s): return True
+        return False
