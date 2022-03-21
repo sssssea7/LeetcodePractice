d250c8fb9321bae6b67716b964f1240da0dfb758
sssssea7 <zhqx9019@gmail.com>
2022-03-21 13:20:35 -0500

    update

--- TwoPointers/763. Partition Labels.py
+++ TwoPointers/763. Partition Labels.py
@@ -1,11 +1,11 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         last = {c: i for i, c in enumerate(s)}
-        j = anchor = 0
+        j = first = 0
         ans = []
         for i, c in enumerate(s):
             j = max(j, last[c])
             if i==j:
-                ans.append(i-anchor+1)
-                anchor = i+1
+                ans.append(i-first+1)
+                first = i+1
         return ans
