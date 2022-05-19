a69620f6f71122aa426b6a150caa6ca31d303965
sssssea7 <zhqx9019@gmail.com>
2022-05-18 23:16:23 -0500

    update

--- DFS/329. Longest Increasing Path in a Matrix.py
+++ DFS/329. Longest Increasing Path in a Matrix.py
@@ -4,11 +4,10 @@ class Solution:
         
         @cache
         def dfs(i, j, val):
-            ans = 1
+            ans = 0
             if 0<=i<len(A) and 0<=j<len(A[0]) and A[i][j]>val:
                 for dx, dy in D:
                     ans = max(ans, 1+dfs(i+dx, j+dy, A[i][j]))
-            else: return 0
             return ans
         
         arr = []
