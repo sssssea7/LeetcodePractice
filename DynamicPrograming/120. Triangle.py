1308df1b8b7d3903c2808b04b6343d6a073ec8f2
sssssea7 <zhqx9019@gmail.com>
2022-06-13 18:04:54 -0500

    update

--- DynamicPrograming/120. Triangle.py
+++ DynamicPrograming/120. Triangle.py
@@ -14,3 +14,10 @@ class Solution:
             if i==len(A) or j==len(A[i]): return 0
             return A[i][j]+min(dfs(i+1, j), dfs(i+1, j+1))
         return dfs(0, 0)
+
+class Solution:
+    def minimumTotal(self, A: List[List[int]]) -> int:
+        for i in range(len(A)-2, -1, -1):
+            for j in range(len(A[i])):
+                A[i][j] = A[i][j]+min(A[i+1][j], A[i+1][j+1])
+        return A[0][0]
