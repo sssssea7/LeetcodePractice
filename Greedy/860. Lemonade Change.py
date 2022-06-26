d5715c56045bbc42e56ca78d1791f8e6528e62f5
sssssea7 <zhqx9019@gmail.com>
2022-06-26 18:24:15 -0500

    update

---
+++ Greedy/860. Lemonade Change.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = ten = 0
+        for b in bills:
+            if b==5: five+=1
+            elif b==10:
+                if not five: return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else: return False
+        return True
