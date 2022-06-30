8b7bcb414a9b0929ff1600e9badd008f506c548d
sssssea7 <zhqx9019@gmail.com>
2022-06-30 15:07:07 -0500

    update

--- Math/462. Minimum Moves to Equal Array Elements II.py
+++ Math/462. Minimum Moves to Equal Array Elements II.py
@@ -2,3 +2,9 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         x = sorted(nums)[len(nums)//2]
         return sum([abs(n-x) for n in nums])
+
+
+class Solution:
+    def minMoves2(self, A: List[int]) -> int:
+        m = median(A)
+        return int(sum([abs(a-m) for a in A]))
