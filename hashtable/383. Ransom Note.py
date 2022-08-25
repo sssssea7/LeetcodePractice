53e6775f8cca298313e444cac5a28caf752aece5
sssssea7 <zhqx9019@gmail.com>
2022-08-24 19:27:36 -0500

    update

--- hashtable/383. Ransom Note.py
+++ hashtable/383. Ransom Note.py
@@ -2,6 +2,13 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         cnt1 = Counter(ransomNote)
         cnt2 = Counter(magazine)
+        return cnt1<=cnt2
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt1 = Counter(ransomNote)
+        cnt2 = Counter(magazine)
         for k, v in cnt1.items():
             if v<=cnt2[k]: continue
             else: return False
